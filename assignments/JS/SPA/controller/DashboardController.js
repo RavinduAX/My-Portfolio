23737e8f5c170b0e8ce78843327d8a2a756238f3
RavinduAX <ravinduadassuriya59@gmail.com>
2022-10-14 02:02:26 +0530

    dashboard customer detail updated

--- assignments/JS/SPA/controller/DashboardController.js
+++ assignments/JS/SPA/controller/DashboardController.js
@@ -1,3 +1,4 @@
+// SPA configuration
 $('#secDash').css('display','block');
 $('#secCust').css('display','none');
 $('#secItem').css('display','none');
@@ -27,3 +28,34 @@ $('#navBtnOrder').click(function (){
     $('#secItem').css('display','none');
     $('#secOrder').css('display','block');
 });
+
+//----------customer details
+// search_customer
+$('#btnDSearch').click(function () {
+    let typedN = $('#txtDInputTelNo').val();
+    let customer = searchCustomer(typedN)
+    if(customer != null){
+        $('#txtDCustID').val(customer.id);
+        $('#txtDInputCustName').val(customer.name);
+    }else{
+        Swal.fire(
+            'Error',
+            'There is no customer available for ' + typedN,
+            'error'
+        )
+    }
+});
+// clear_fields
+$('#btnDCNew').click(function () {
+    $('#txtDInputTelNo').val("");
+    $('#txtDCustID').val("");
+    $('#txtDInputCustName').val("");
+});
+function searchCustomer(contact){
+    for (let customer of customerArr) {
+        if(customer.contact === contact){
+            return customer;
+        }
+    }
+    return null;
+}
