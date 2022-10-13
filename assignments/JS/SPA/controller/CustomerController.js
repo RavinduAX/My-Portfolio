bb0502f7c2d80316ea8d0de90504c59af6ae9b06
RavinduAX <ravinduadassuriya59@gmail.com>
2022-10-13 12:30:07 +0530

    loadallCustomers function added

--- assignments/JS/SPA/controller/CustomerController.js
+++ assignments/JS/SPA/controller/CustomerController.js
@@ -4,3 +4,35 @@ $('#btnCNew').click(function () {
     $('#txtCAddress').val("");
     $('#txtCContactNo').val("");
 });
+
+$('#btnCSave').click(function () {
+    let customerId = $('#txtCInputCustomerID').val();
+    let customerName = $('#txtCCustomerName').val();
+    let customerAddress = $('#txtCAddress').val();
+    let customerContact = $('#txtCContactNo').val();
+
+    customerObj.id = customerId;
+    customerObj.name = customerName;
+    customerObj.address = customerAddress;
+    customerObj.contact = customerContact;
+
+    customerArr.push(customerObj);
+
+    loadAllCustomers();
+
+
+});
+
+$('#btnCGetAll').click(function () {
+    loadAllCustomers();
+});
+
+function loadAllCustomers(){
+    $('#tblCustomer').empty();
+
+    for (let customer of customerArr) {
+        let row = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.contact}</td></tr>`;
+        $('#tblCustomer').append(row);
+    }
+
+}
